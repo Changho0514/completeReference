d834b2929c84576140f904050dc8d5179b975410
Changho0514 <chh9450@naver.com>
2023-05-02 19:00:20 +0900

    Push 필요합니다.

--- javabook/Intelli/src/main/java/CompleteReference/FlowEx33.java
+++ javabook/Intelli/src/main/java/CompleteReference/FlowEx33.java
@@ -6,9 +6,9 @@ public class FlowEx33 {
         for (int i = 2; i <= 9; i++) {
             for (int j = 1; j <= 9; j++) {
                 if (j == 5)
-                    break Loop1;
+//                    break Loop1;
 //                    break;
-//                    continue Loop1;
+                    continue Loop1;
 //                    continue;
                 System.out.println(i + "*" + j + "=" + i * j);
                 // jb 추가용
