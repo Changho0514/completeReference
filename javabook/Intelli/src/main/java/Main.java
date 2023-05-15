ee1f75e3c2706b2b389d6fed55312fdbccce9bad
Changho Choi <chh9450@naver.com>
2023-05-15 23:51:19 +0900

    Update Main.java

--- javabook/Intelli/src/main/java/Main.java
+++ javabook/Intelli/src/main/java/Main.java
@@ -1,4 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         System.out.println("n번째 클래스 시행결과");
     }
 }
